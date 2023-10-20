2f4e1b15bd4884ab8bd8325f077ffd7dc24e8f52
DigitallyIntrinsic <139934612+DigitallyIntrinsic@users.noreply.github.com>
2023-10-20 15:42:03 -0500

    edited file routes and added html routers

--- routes/index.js
+++
@@ -1,7 +0,0 @@
-// Creates a mini instance of an express server
-const router = require('express').Router();
-
-// Import our files containing our routes
-const dbRouter = require('./db');
-
-module.exports = router;
