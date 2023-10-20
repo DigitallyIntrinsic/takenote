f09f446c838edc8f5c58ce52b9a67f4215639e78
DigitallyIntrinsic <139934612+DigitallyIntrinsic@users.noreply.github.com>
2023-10-20 15:05:00 -0500

    function to write data to file

--- server.js
+++ server.js
@@ -49,3 +49,9 @@ const readThenAppendToJson = (content, file) => {
         }
     });
 };
+
+// Writes data to db.json file
+const writeNewNoteToJson = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
