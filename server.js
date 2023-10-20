2ff64993ad30bbe89c4e64d5150d744d4742434d
DigitallyIntrinsic <139934612+DigitallyIntrinsic@users.noreply.github.com>
2023-10-20 15:04:01 -0500

    Finalize user input data function

--- server.js
+++ server.js
@@ -42,3 +42,10 @@ const readThenAppendToJson = (content, file) => {
     fs.readFile(file, "utf8", (err, data) => {
         if (err) {
             console.error(err);
+        } else {
+            const parsedData = JSON.parse(data);
+            parsedData.push(content);
+            writeNewNoteToJson(file, parsedData);
+        }
+    });
+};
